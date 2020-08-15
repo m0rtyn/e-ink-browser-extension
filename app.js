76d8edcf9a5bbce231e5336ec08ef49efd53bd6c
Martyn <zogacc@gmail.com>
2020-08-15 19:05:02 +0300

    update filters

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-const filters = 'brightness(1) grayscale(1) invert()'
+const filters = 'brightness(0.97) grayscale(1) contrast(0.97)'
 const pageRootStyles = document.documentElement.style
-pageRootStyles.filter += ` ${filters}`; 
+pageRootStyles.filter += ` ${filters}`;
